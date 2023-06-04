27b695003bb1301326fd9b2fa1d484f460507ce1
fabiohidekihirose <fabiohidekihirose@gmail.com>
2023-06-04 22:27:59 +0900

    Carousel for Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,72 @@
+"use client";
+
 import Image from "next/image";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import Carousel, { CarouselItem } from "@/components/Carousel";
+
+const carouselItems = [{}];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        {/* <Image
-          src={"/images/products/sofa1.png"}
-          alt="sofa1"
-          width={1000}
-          height={500}
-        /> */}
+    <main className="flex flex-col items-center justify-between p-24">
+      <Carousel infiniteLoop={true} showArrows={true} className="w-[30%]">
+        <div className="w-full h-full object-fill">
+          <img src="/images/products/sofa1.jpg"></img>
+        </div>
+        <div>
+          <img src="/images/products/sofa2.jpg"></img>
+        </div>
+        <div>
+          <img src="/images/products/sofa3.jpg"></img>
+        </div>
+      </Carousel>
+      {/* <div className="w-[49%]">
+        <Carousel>
+          <CarouselItem>
+            <Image
+              alt="sofa1"
+              src={"/images/products/sofa1.jpg"}
+              width="0"
+              height="0"
+              sizes="100vw"
+              className="w-full h-auto"
+            ></Image>
+          </CarouselItem>
+          <CarouselItem>
+            <Image
+              alt="sofa2"
+              src={"/images/products/sofa2.jpg"}
+              width="0"
+              height="0"
+              sizes="100vw"
+              className="w-full h-auto"
+            ></Image>
+          </CarouselItem>
+          <CarouselItem>
+            <Image
+              alt="sofa3"
+              src={"/images/products/sofa3.jpg"}
+              width="0"
+              height="0"
+              sizes="100vw"
+              className="w-full h-auto"
+            ></Image>
+          </CarouselItem>
+        </Carousel> */}
+      {/* </div> */}
+      <div className="w-[49%]">
+        <Image
+          alt="sofa4"
+          src={"/images/products/sofa4.jpg"}
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="w-full h-auto"
+        ></Image>
+        <div></div>
       </div>
-      <div></div>
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
     </main>
   );
 }
---
+++ components/Logo.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <Link href={"/"}>
+      <div className="flex items-center ml-[40px]">
+        <Image
+          src={"/images/home_decor_logo.png"}
+          alt="logo"
+          width={70}
+          height={70}
+        ></Image>
+        <p className="text-[#30628B] font-['Poppins', sans-serif] font-[500] tracking-[.25em]">
+          HOMEDECOR
+        </p>
+      </div>
+    </Link>
+  );
+}
