e80ecb15a769f7127157897bc81e8b152b680116
fabiohidekihirose <fabiohidekihirose@gmail.com>
2023-06-07 22:29:23 +0900

    Added dummy data and images

---
+++ data.tsx
@@ -0,0 +1,151 @@
+export const carouselItems = [
+  {
+    src: "/images/carousel-items/carousel-item-1.png",
+  },
+  {
+    src: "/images/carousel-items/carousel-item-2.png",
+  },
+  {
+    src: "/images/carousel-items/carousel-item-1.png",
+  },
+];
+
+export const cards1 = [
+  {
+    src: "/images/products/sofa4.jpg",
+  },
+  {
+    src: "/images/products/sofa4.jpg",
+  },
+];
+
+export const cards2 = [
+  {
+    src: "/images/products/sofa4.jpg",
+  },
+  {
+    src: "/images/products/sofa4.jpg",
+  },
+  {
+    src: "/images/products/sofa4.jpg",
+  },
+];
+
+export const departments = [
+  {
+    label: "Living Room",
+    image: "/images/departments/living-room.jpg",
+    department: "living-room",
+  },
+  {
+    label: "Bedroom",
+    image: "/images/departments/bedroom.jpg",
+    department: "bedroom",
+  },
+  {
+    label: "Dining Room",
+    image: "/images/departments/dining-room.jpg",
+    department: "dining-room",
+  },
+  {
+    label: "Home Office",
+    image: "/images/departments/home-office.jpg",
+    department: "home-office",
+  },
+  {
+    label: "Outdoor",
+    image: "/images/departments/outdoor.jpg",
+    department: "outdoor",
+  },
+  {
+    label: "Kids' Furniture",
+    image: "/images/departments/kids-furniture.jpg",
+    department: "kids-furniture",
+  },
+  {
+    label: "Storage & Organization",
+    image: "/images/departments/store-furniture.jpg",
+    department: "store-organization",
+  },
+  {
+    label: "Lighting",
+    image: "/images/departments/lighting.jpg",
+    department: "lighting",
+  },
+  {
+    label: "Decor & Accessories",
+    image: "/images/departments/decoration.jpg",
+    department: "decor-accessories",
+  },
+  {
+    label: "Special Offers",
+    image: "/images/departments/special-offers.jpg",
+    department: "special-offers",
+  },
+];
+
+export const products = [
+  {
+    id: 1,
+    name: "Small Green Sofa",
+    image: "/images/products/sofa/small-green-sofa.jpg",
+    price: 10000,
+    department: "living-room",
+  },
+  {
+    id: 2,
+    name: "Yellow Armchair",
+    image: "/images/products/sofa/yellow-armchair.jpg",
+    price: 10000,
+    department: "living-room",
+  },
+  {
+    id: 3,
+    name: "Small Grey Sofa",
+    image: "/images/products/sofa/small-grey-sofa.jpg",
+    price: 10000,
+    department: "living-room",
+  },
+  {
+    id: 4,
+    name: "Large Black Sofa",
+    image: "/images/products/sofa/large-black-sofa.jpg",
+    price: 10000,
+    department: "living-room",
+  },
+  {
+    id: 5,
+    name: "Large White Bed",
+    image: "/images/products/bed/large-white-bed.jpg",
+    price: 30000,
+    department: "bedroom",
+  },
+  {
+    id: 6,
+    name: "Large White Bed 2",
+    image: "/images/products/bed/large-white-bed-2.jpg",
+    price: 40000,
+    department: "bedroom",
+  },
+  {
+    id: 7,
+    name: "Large White Bed 3",
+    image: "/images/products/bed/large-white-bed-3.jpg",
+    price: 50000,
+    department: "bedroom",
+  },
+  {
+    id: 8,
+    name: "Large White Bed 4",
+    image: "/images/products/bed/large-white-bed-4.jpg",
+    price: 100000,
+    department: "bedroom",
+  },
+  {
+    id: 9,
+    name: "Large Grey Bed",
+    image: "/images/products/bed/large-grey-bed.jpg",
+    price: 30000,
+    department: "bedroom",
+  },
+];
