a0131a8bf976eea1cdf705247e17a7cdebb86fdc
fabiohidekihirose <fabiohidekihirose@gmail.com>
2023-06-08 09:15:29 +0900

    Feat: About Us Page

---
+++ app/about-us/page.tsx
@@ -0,0 +1,69 @@
+export default function AboutUs() {
+  return (
+    <div className="p-24 pt-28 text-[#000000] space-y-[30px]">
+      <img src="/images/about-us-1.jpg"></img>
+      <div className="text-[40px] font-[700] flex flex-col items-center">
+        About Us
+      </div>
+      <div className="flex space-x-[2%] text-[18px] mb-[20px]">
+        <div className="w-[49%] space-y-[10px]">
+          <p>
+            Welcome to HomeDecor, your premier destination for exceptional
+            furniture and home decor solutions. At HomeDecor, we believe that a
+            well-furnished home is a reflection of your unique style and
+            personality. We are passionate about helping you create spaces that
+            are not only functional but also exude comfort, elegance, and a
+            sense of warmth.
+          </p>
+          <p>
+            With a vast selection of high-quality furniture pieces and
+            accessories, HomeDecor offers a comprehensive range of options to
+            suit every room in your home. Whether you're looking to furnish your
+            living room, bedroom, dining area, home office, or outdoor space, we
+            have carefully curated collections to cater to your specific needs
+            and design preferences.
+          </p>
+          <p>
+            Our dedicated team of furniture experts is committed to providing
+            you with personalized assistance every step of the way. We
+            understand that choosing the perfect furniture can be overwhelming,
+            so we're here to guide you through the selection process, offering
+            valuable advice and recommendations based on your individual taste,
+            budget, and lifestyle.
+          </p>
+          <p>
+            At HomeDecor, we source our products from reputable manufacturers
+            known for their craftsmanship, durability, and attention to detail.
+            From luxurious sofas and stylish dining sets to cozy beds and
+            functional storage solutions, our furniture pieces are thoughtfully
+            designed to enhance your living spaces, ensuring both aesthetics and
+            functionality.
+          </p>
+        </div>
+        <div className="w-[49%] space-y-[10px] mt-0">
+          <p>
+            In addition to our extensive furniture offerings, we also offer an
+            array of home decor accents, including lighting fixtures, rugs, art
+            pieces, and decorative accessories. These carefully curated items
+            will add the finishing touches to your home, allowing you to infuse
+            your personal style and create a cohesive and inviting atmosphere.
+          </p>
+          <p>
+            We understand the importance of a seamless shopping experience,
+            which is why we offer convenient online browsing and secure
+            purchasing options. Additionally, we provide reliable delivery
+            services to ensure that your furniture arrives safely and promptly
+            at your doorstep.
+          </p>
+          <p>
+            At HomeDecor, our mission is to transform your house into a home
+            that truly reflects your taste and aspirations. Step into our
+            virtual showroom or visit our physical store to discover the endless
+            possibilities of creating a space you'll love coming home to.
+            Experience the art of fine living with HomeDecor.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
