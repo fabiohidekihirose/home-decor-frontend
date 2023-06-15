5468ed9b9742b763a57e06c9de83d4e1173f3ba0
fabiohidekihirose <fabiohidekihirose@gmail.com>
2023-06-15 15:35:34 +0900

    Update: Search Bar functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,10 +7,15 @@ import { useState, useEffect } from "react";
 import { RiAccountCircleLine, RiShoppingCart2Line } from "react-icons/ri";
 import { DepartmentProps } from "@/types";
 import AccountMenu from "./AccountMenu";
+import { useRouter } from "next/navigation";
+import { useAppSelector } from "@/redux/hooks";
+import { selectCurrentUser } from "@/redux/slicers/authSlice";
 
 export default function Navbar() {
   const [departments, setDepartments] = useState([]);
   const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const router = useRouter();
+  const user = useAppSelector(selectCurrentUser);
 
   const baseUrl = "http://localhost:8000";
 
@@ -32,17 +37,26 @@ export default function Navbar() {
     setShowAccountMenu(false);
   };
 
+  const pressEnterHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      router.push(
+        `/products?search=${(event.target as HTMLInputElement).value}`
+      );
+    }
+  };
+
   return (
-    <div className="fixed space-y-[10px] w-full py-[10px] z-50 bg-[#ffffff] border-b-[1px] shadow-[0_4px_30px_rgba(157,157,157,0.25)]">
+    <div className="fixed items-center flex flex-col space-y-[10px] w-full py-[10px] z-50 bg-[#ffffff] border-b-[1px] shadow-[0_4px_30px_rgba(157,157,157,0.25)]">
       <div className="flex items-center space-x-[2%] w-full border-b-[1px]">
         <Logo />
         <input
           placeholder="Search for a product..."
           className="w-[50%] h-[40px] rounded-[10px] p-4 border-[#30628b] border-[2px]"
+          onKeyUp={pressEnterHandler}
         ></input>
         <div className="flex items-center absolute right-[50px]">
           <Link
-            href={"/account"}
+            href={user ? "/account" : "/login"}
             className="ml-[20px] flex flex-col items-center hover:text-[#30628b] text-[#000000]"
             onMouseOver={mouseOverHandler}
             onMouseOut={mouseOutHandler}
@@ -65,7 +79,7 @@ export default function Navbar() {
           </Link>
         </div>
       </div>
-      <div className="flex space-x-[2%] flex-wrap">
+      <div className="flex space-x-[30px] flex-wrap mx-[min(20px)] justify-center">
         {departments.map((department: DepartmentProps) => (
           <Link
             key={department.id}
@@ -73,7 +87,7 @@ export default function Navbar() {
               pathname: `/products`,
               query: { department: department.department },
             }}
-            className="hover:text-[#30628b] hover:underline"
+            className="hover:text-[#30628b] hover:underline text-left"
           >
             {department.label}
           </Link>
