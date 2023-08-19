972474a62aa630bc6476241ba021f729ef8eb73b
fabiohidekihirose <fabiohidekihirose@gmail.com>
2023-08-19 23:33:59 +0900

    Feat: it's possible to add to cart from favorite page

--- app/favorite/components/FavoriteItemCard.tsx
+++ app/favorite/components/FavoriteItemCard.tsx
@@ -3,10 +3,22 @@ import { ItemCardProps } from "@/types";
 import Link from "next/link";
 import Image from "next/image";
 import { removeFavorite } from "@/redux/slicers/favoriteSlice";
+import { addToCart } from "@/redux/slicers/cartSlice";
 import { useAppDispatch } from "@/redux/hooks";
+import { useState } from "react";
+import { BiCartAdd } from "react-icons/bi";
+import { useRouter } from "next/navigation";
+import { ProductProps } from "@/types";
 
 export default function FavoriteItemCard({ item }: ItemCardProps) {
+  const [counterItem, setCounterItem] = useState(1);
   const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  const addToCartHandler = (product: ProductProps) => {
+    dispatch(addToCart({ ...product, inCart: counterItem }));
+    router.push("/cart");
+  };
 
   return (
     <div className="flex h-[210px] space-x-[20px] p-[20px] rounded-[10px] shadow-[0_4px_30px_rgba(157,157,157,0.25)]">
@@ -30,20 +42,29 @@ export default function FavoriteItemCard({ item }: ItemCardProps) {
         <div className="flex">
           <button
             className="border-[1px] border-[#000000] p-2 rounded-l-[10px] hover:bg-[#30628b] hover:text-[#ffffff]"
-            // onClick={() => dispatch(decrementInCart({ id: item.id }))}
+            onClick={() =>
+              setCounterItem((curr) => (curr === 1 ? 1 : curr - 1))
+            }
           >
             -
           </button>
           <div className="text-center border-y-[1px] border-[#000000] w-[20%] p-2">
-            {item.inCart}
+            {counterItem}
           </div>
           <button
             className="border-[1px] border-[#000000] p-2 rounded-r-[10px] hover:bg-[#30628b] hover:text-[#ffffff]"
-            // onClick={() => dispatch(incrementInCart({ id: item.id }))}
+            onClick={() => setCounterItem((curr) => curr + 1)}
           >
             +
           </button>
         </div>
+        <button
+          className="flex items-center space-x-[5px] bg-[#30628B] text-[#ffffff] p-2 rounded-[10px] hover:bg-[#4186BE]"
+          onClick={() => addToCartHandler(item)}
+        >
+          <BiCartAdd></BiCartAdd>
+          <p>Add to cart</p>
+        </button>
       </div>
       <div className="grow flex flex-col items-end space-y-[90px]">
         <div className="text-[30px]">
--- app/favorite/page.tsx
+++ app/favorite/page.tsx
@@ -10,10 +10,10 @@ export default function Favorite() {
   );
 
   return (
-    <div className="flex max-lg:flex-col max-lg:p-6 p-24 max-lg:pt-36 lg:pt-44 xl:pt-36 lg:space-x-[2%] text-[#000000] shadow-[0_4px_30px_rgba(157,157,157,0.25)] max-lg:space-y-[30px]">
-      <div className="lg:w-[64%] space-y-[30px]">
+    <div className="flex max-lg:p-6 p-24 max-lg:pt-36 lg:pt-44 xl:pt-36 lg:space-x-[2%] text-[#000000] shadow-[0_4px_30px_rgba(157,157,157,0.25)] max-lg:space-y-[30px] w-full">
+      <div className="space-y-[30px] w-full">
         <h2 className="text-[40px] font-[500]">Favorite Items</h2>
-        <div className="space-y-[20px]">
+        <div className="space-y-[20px] w-full">
           {favoriteItems.length
             ? favoriteItems.map((favoriteItem: ProductProps) => (
                 <FavoriteItemCard item={favoriteItem} key={favoriteItem.id} />
